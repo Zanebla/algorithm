330799c33483837fd98ce7d10267df9acff2060d
Zane <3277696440@qq.com>
2023-12-25 11:08:01 +0800

    brace_match

--- Python/Tsinghua/stack.py
+++ Python/Tsinghua/stack.py
@@ -15,9 +15,28 @@ class Stack:
         else:
             return None
 
+    def is_empty(self):
+        return len(self.stack) == 0
 
-stack = Stack()
-stack.push(1)
-stack.push(2)
-stack.push(3)
-print(stack.pop())
+
+def brace_match(s):
+    match = {')': '(', ']': '[', '}': '{'}
+    stack = Stack()
+    for ch in s:
+        if ch in {'(', '[', '{'}:
+            stack.push(ch)
+        else:   # ch in {')', ']', '}'}
+            if stack.is_empty():
+                return False
+            elif stack.get_top() == match[ch]:
+                stack.pop()
+            else:  # stack.get_top() != match[ch]
+                return False
+    if stack.is_empty():
+        return True
+    else:
+        return False
+
+
+print(brace_match('[{}()[({}){}]{()}[]]'))
+print(brace_match('[{]}'))
